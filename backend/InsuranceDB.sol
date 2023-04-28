f390b9f82fe85189a988a0e73bb683d90408ee14
kavinkumar2404 <87380595+kavinkumar2404@users.noreply.github.com>
2023-04-28 12:36:34 +0530

    init

---
+++ backend/InsuranceDB.sol
@@ -0,0 +1,42 @@
+pragma solidity 0.8.7;
+
+contract InsuranceDB {
+    struct InsurancePolicy {
+        string policyNumber;
+        string insurerName;
+        uint256 coverageAmount;
+        uint256 premiumAmount;
+        uint256 startDate;
+        uint256 endDate;
+    }
+
+    mapping (address => InsurancePolicy[]) private policies;
+
+    function addPolicy(
+        string memory policyNumber,
+        string memory insurerName,
+        uint256 coverageAmount,
+        uint256 premiumAmount,
+        uint256 startDate,
+        uint256 endDate
+    ) public {
+        InsurancePolicy memory policy = InsurancePolicy(
+            policyNumber,
+            insurerName,
+            coverageAmount,
+            premiumAmount,
+            startDate,
+            endDate
+        );
+
+        policies[msg.sender].push(policy);
+    }
+
+    function getPolicies(address policyHolder) public view returns (InsurancePolicy[] memory) {
+        return policies[policyHolder];
+    }
+
+    function getPolicy(address policyHolder, uint256 policyIndex) public view returns (InsurancePolicy memory) {
+        return policies[policyHolder][policyIndex];
+    }
+}
